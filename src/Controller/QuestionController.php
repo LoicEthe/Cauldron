fbfd6cef31581a02abd3973cb3aae425a94105ee
LOIC ETHEVE <loic.ethve@gmail.com>
2021-10-25 14:31:28 +0200

    Third commit

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,19 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('Wouhouu');
+        return $this->render('question/homepage.html.twig');
     }
 
     /**
-     * @Route("/questions/{slug}")
+     * @Route("/questions/{slug}", name="app_question_show")
      */
     public function show($slug)
     {
